ab825c6286b3f7ae54d950d87127b1e589dfd59a
LukasRolls <lukasrolland57@gmail.com>
2024-12-19 17:16:38 +0100

    Mise en place du Portfolio

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const options = {
+        strings: ["Developpeur", "Apprenti", "Langatte"],
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 1000,
+        loop: true,
+    };
+    new Typed("#typed-text", options);
+});
+
