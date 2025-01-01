13f0822751ff96c296d62d97c9c2622770cb289c
LukasRolls <lukasrolland57@gmail.com>
2025-01-01 21:13:10 +0100

    Maj en utilisant moins bootstrap

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,3 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
     };
     new Typed("#typed-text", options);
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    AOS.init(); 
+});
